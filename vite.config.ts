4adbe2fe6f6ab212f20e299287def134a2f83057
pavitra0 <143537026+pavitra0@users.noreply.github.com>
2024-12-15 19:22:50 +0530

    Add files via upload

---
+++ vite.config.ts
@@ -0,0 +1,18 @@
+// https://vite.dev/config/
+
+import path from "path";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    sourcemap: false,
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  
+});
